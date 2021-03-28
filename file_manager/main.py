8cf586a56092651c0c2f9c37a15ebb126c1f0b8d
Ricardo Gottheil <ricardogottheil@gmail.com>
2021-03-28 13:30:49 -0500

    [ADD] Method to read log files in FileManager class

--- file_manager/main.py
+++ file_manager/main.py
@@ -21,6 +21,15 @@ class FileManager:
         file = open("%s/logs/%s" %(os.getcwd(), self.file), 'a')
         file.write(info + "\n")
         file.close()
+    
+    def readLogFile(self):
+        file = open("%s/logs/%s" %(os.getcwd(), self.file), 'r')
+        lines = file.readlines()
+
+        count = 0
+        for line in lines:
+            count += 1
+            print("[{}]: {}".format(count, line.strip()))
 
     def listLogs(self):
         os.system("ls %s/logs/" %(os.getcwd()))
