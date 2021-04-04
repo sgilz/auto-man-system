1bae90c945544eb781025215085f7c99f17ec31d
Ricardo Gottheil <ricardogottheil@gmail.com>
2021-04-03 20:27:17 -0500

    First implementation of GUI

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,6 +1,3 @@
-# TODO: Add info to install tkinter in setup.py
-# python -m pip install tk
-
 import tkinter 
 from tkinter import ttk
 
@@ -37,8 +34,77 @@ tv.insert('', 'end', value=(
 tab2 = ttk.Frame(tab_control)
 tab_control.add(tab2, text="File Manager")
 
-lbl1 = tkinter.Label(tab2, text= 'File Manager')
-lbl1.grid(column=0, row=0)
+
+# Change FIle name
+# =================
+def changeFileName():
+    if len(nameFileName.get()) > 0:
+        labelFileName.configure(text= 'New File Name: \n' + nameFileName.get())
+ 
+labelFileName = ttk.Label(tab2, text = "File Name: Logs-1.log")
+labelFileName.grid(column = 0, row = 0, pady=8)
+ 
+nameFileName = tkinter.StringVar()
+nameFileNameEntered = ttk.Entry(tab2, width = 15, textvariable = nameFileName)
+nameFileNameEntered.grid(column = 0, row = 1)
+
+button1 = tkinter.Button(tab2, text="Change File Name", command = changeFileName)
+button1.grid(column=0, row=2, padx=8, pady=8)
+
+
+# Create Directory
+# =================
+def createDirectory():
+    if len(nameCreateDirectory.get()) > 0:
+        labelCreateDirectory.configure(text= 'Directory ' + nameCreateDirectory.get() + '\n created')
+ 
+labelCreateDirectory = ttk.Label(tab2, text = "Create new directory")
+labelCreateDirectory.grid(column = 1, row = 0, pady=8)
+ 
+nameCreateDirectory = tkinter.StringVar()
+nameCreateDirectoryEntered = ttk.Entry(tab2, width = 15, textvariable = nameCreateDirectory)
+nameCreateDirectoryEntered.grid(column = 1, row = 1)
+
+button2 = tkinter.Button(tab2, text="Create directory", command = createDirectory)
+button2.grid(column=1, row=2, padx=10, pady=8)
+
+# Delete Directory
+# =================
+def deleteDirectory():
+    if len(nameDeleteDirectory.get()) > 0:
+        labelDeleteDirectory.configure(text= 'Directory ' + nameDeleteDirectory.get() + '\n deleted')
+ 
+labelDeleteDirectory = ttk.Label(tab2, text = "Delete directory")
+labelDeleteDirectory.grid(column = 2, row = 0, pady=8)
+ 
+nameDeleteDirectory = tkinter.StringVar()
+nameDeleteDirectoryEntered = ttk.Entry(tab2, width = 15, textvariable = nameDeleteDirectory)
+nameDeleteDirectoryEntered.grid(column = 2, row = 1)
+
+button3 = tkinter.Button(tab2, text="Delete directory", command = deleteDirectory)
+button3.grid(column=2, row=2, padx=10, pady=8)
+
+# LS 
+# =================
+def ListLogs():
+    logsArea1.insert(tkinter.END, 'List logs \n')
+
+def ListLogsDirectory():
+    logsArea2.insert(tkinter.END, 'List logs Directory \n')
+
+
+button4 = tkinter.Button(tab2, text="List logs", command = ListLogs)
+button4.grid(column=3, row=0, padx=10, pady=8)
+
+button5 = tkinter.Button(tab2, text="List directory", command = ListLogsDirectory)
+button5.grid(column=3, row=1, padx=10, pady=8)
+
+logsArea1 = tkinter.Text(tab2, height=15, width=50)
+logsArea1.grid(column=0, row=5, columnspan=2, rowspan=2, padx=10, pady=10)
+
+logsArea2 = tkinter.Text(tab2, height=15, width=50)
+logsArea2.grid(column=2, row=5, columnspan=2, rowspan=2, padx=10, pady=10)
+
 
 #Complete config tabs
 tab_control.pack(expand=1, fill='both') 
