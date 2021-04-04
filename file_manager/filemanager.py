3dca840c721e07e258d4000ee346f3387780fb28
Ricardo Gottheil <ricardogottheil@gmail.com>
2021-04-04 18:13:11 -0500

    Return instead print in filemanager file

--- file_manager/filemanager.py
+++ file_manager/filemanager.py
@@ -11,17 +11,20 @@ class FileManager:
 
     def createLog(self):
         open("%s/logs/%s" %(self.root, self.file), 'a').close()
+        return "Log created"
     
     def deleteLogFile(self):
         if os.path.exists("%s/logs/%s" %(self.root, self.file)):
             os.remove("%s/logs/%s" %(self.root, self.file))
+            return "Log deleted"
         else:
-            print("File does not exit")
+            return "File does not exit"
     
     def addLineLog(self, info = ""):
         file = open("%s/logs/%s" %(self.root, self.file), 'a')
         file.write(info + "\n")
         file.close()
+        return "Line log added"
     
     def readLogFile(self):
         file = open("%s/logs/%s" %(self.root, self.file), 'r')
@@ -32,41 +35,48 @@ class FileManager:
         for line in lines:
             count += 1
             print("[{}]: {}".format(count, line.strip()))
+        
+        return "Logs readed"
 
     def listLogs(self):
         os.system("ls %s/logs/" %(self.root))
+        return "Logs listed"
 
     def createDir(self, nameDir):
         path = "{}/{}".format(self.root, nameDir)
         try:
             os.mkdir(path)
         except OSError:
-            print ("Creation of the directory %s failed" % path)
+            return ("Creation of the directory %s failed" % path)
         else:
-            print ("Successfully created the directory %s " % path)
+            return ("Successfully created the directory %s " % path)
 
     def deleteDir(self, nameDir):
         path = "{}/{}".format(self.root, nameDir)
         try:
             os.rmdir(path)
         except OSError:
-            print ("Delete of the directory %s failed" % path)
+            return ("Delete of the directory %s failed" % path)
         else:
-            print ("Successfully deleted the directory %s " % path)
+            return ("Successfully deleted the directory %s " % path)
 
     def addFileInDirectory(self, nameDir):
         open("%s/%s/%s" %(self.root, nameDir,self.file), 'a').close()
+        return "File in drectory added"
     
     def addLineInFileDirectory(self, nameDir, info = ""):
         file = open("%s/%s/%s" %(self.root, nameDir,self.file), 'a')
         file.write(info + "\n")
         file.close()
+        return "Line in file in directory added"
     
     def listDirectoriesInDirectory(self, nameDir):
         os.system("ls %s/%s/" %(self.root, nameDir))
+        return "Directories in directory listed"
 
     def setFileName(self, fileName):
         self.file = fileName
+        return "File name setted"
     
     def getFileName(self):
         return self.file
